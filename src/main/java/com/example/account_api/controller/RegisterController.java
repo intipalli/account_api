81aa0b8678cb743ced3e837469cf522f38787fb1
Mitchell <mbailey54@student.gsu.edu>
2024-08-06 15:18:22 +0000

    implemented  register controller

--- src/main/java/com/example/account_api/controller/RegisterController.java
+++ src/main/java/com/example/account_api/controller/RegisterController.java
@@ -1,6 +1,13 @@
 package com.example.account_api.controller;
 
 import java.net.URI;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,12 +29,54 @@ public class RegisterController {
             return ResponseEntity.badRequest().build();
         }
 
-        postNewCustomerToCustomerAPI();
+        postNewCustomer(newCustomer);
 
-        //newCustomer = customersRepository.save(newCustomer);
+       
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(newCustomer.getId()).toUri();
         ResponseEntity<?> response = ResponseEntity.created(location).build();
         return response;
     }
+
+    private void postNewCustomer(String json) {
+		try {
+
+			URL url = new URL("http://localhost:8080/api/customers");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+	  		Token token = TokenAPI.getAppUserToken();
+	  		conn.setRequestProperty("authorization", "Bearer " + token.getToken());
+	  		// conn.setRequestProperty("tokencheck", "false");
+
+			OutputStream os = conn.getOutputStream();
+			os.write(json.getBytes());
+			os.flush();
+
+			if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
 }
